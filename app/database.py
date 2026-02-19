a25fe5327d94da73ad03e4c5e1efe10a6a736f50
Shivani Kamble <kambleshivani78@gmail.com>
2026-02-19 15:22:19 +0530

    fixed database for render

--- app/database.py
+++ app/database.py
@@ -1,17 +1,33 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./task_manager.db"
+# DATABASE_URL = "sqlite:///./task_manager.db"
+
+# engine = create_engine(
+#     DATABASE_URL,
+#     connect_args={"check_same_thread": False}
+# )
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# Base = declarative_base()
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./tasks.db"
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
