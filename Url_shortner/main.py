fd79f3d1922620c9aa8d2e51f173d54f3632fba0
Shivani Kamble <kambleshivani78@gmail.com>
2026-02-19 14:38:20 +0530

    Removed Url_shortner project

--- Url_shortner/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from database import engine, Base
-from routers.shortener import router as shortener_router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="URL Shortener")
-app.include_router(shortener_router)
-
